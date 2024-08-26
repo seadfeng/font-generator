6dd589a4e35a71cc522806c44964b558fd8cf43a
Sead Feng <seadfeng@gmail.com>
2024-08-26 15:48:48 +0800

    update footer link

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
       <span>
-        © {new Date().getFullYear()} <a href="https://www.font-generator.org/">{appConfig.appName}</a>. All rights reserved.
+        © {new Date().getFullYear()} <a href="https://www.fontgenerator.dev/">{appConfig.appName}</a>. All rights reserved.
       </span>
     </footer>
   );

7d1a166e966c493344aa8f21495d74b71817c015
Sead Feng <seadfeng@gmail.com>
2024-10-24 15:57:11 +0800

    update footer

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -14,6 +14,14 @@ export function Footer() {
           <img alt="Favicon Extractor" src="https://www.faviconextractor.com/favicon/www.faviconextractor.com" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
           <span>Favicon Extractor</span>
         </a>
+        <a href="https://www.favicongenerator.io/" className="text-primary flex items-center">
+          <img alt="Favicon Generator" src="https://www.faviconextractor.com/favicon/www.favicongenerator.io" className="h-5 w-5 mr-1 rounded-full bg-secondary" width="20" height="20" />
+          <span>Favicon Generator</span>
+        </a>
+        <a href="https://www.linkgowhere.com/" className="text-primary flex items-center">
+          <img alt="LinkGoWhere" src="https://www.faviconextractor.com/favicon/www.linkgowhere.com" className="h-5 w-5 mr-1 rounded-full bg-secondary" width="20" height="20" />
+          <span>LinkGoWhere</span>
+        </a>
       </div>
       <span>
         © {new Date().getFullYear()} <a href="https://www.fontgenerator.dev/">{appConfig.appName}</a>. All rights reserved.

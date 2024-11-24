ab0101b88fe3a24d1c42e45ba9d6833b39e5b710
Sead Feng <seadfeng@gmail.com>
2024-11-24 20:53:24 +0800

    fix footer ui

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -4,7 +4,7 @@ import { appConfig } from "@/config";
 export function Footer() {
   return (
     <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
-      <div className="my-3 flex items-center justify-center gap-3">
+      <div className="my-3 flex flex-wrap items-center justify-center gap-3">
         <b>Our Products: </b>
         <a href="https://www.redirectchecker.org/" className="text-primary flex items-center">
           <img alt="Redirect Checker" src="https://www.faviconextractor.com/favicon/www.redirectchecker.org" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
